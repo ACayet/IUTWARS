ba04884b9ab541f462e339c42e502e6310b01651
Solok the Baguette Boy <solok-sardine@hotmail.com>
2018-03-13 11:36:26 +0100

    Avancée système de combat 3
    - Objet vide pour gérer le combat et les clicks
    - Système pensé pour inclure plusieurs persos joueurs
    - On peut récupéré l'ennemi sur lequel on a cliqué

---
+++ Assets/AssetsClassroom/Scripts/ClickManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour {
+
+    GameObject currentClicked = null;
+    GameObject[] ennemies = null;
+    GameObject[] heroes = null;
+
+	// Use this for initialization
+	void Start () {
+		heroes = GameObject.FindGameObjectsWithTag("Player");
+        ennemies = GameObject.FindGameObjectsWithTag("Ennemy");
+        changeSelected(heroes[0]);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            Debug.Log("Mouse Clicked at " + mousePos.x + " " + mousePos.y);
+            if (hit.collider != null)
+            {
+                Debug.Log("Something with the tag " + hit.collider.tag + " was clicked! at " + mousePos.x + " " + mousePos.y);
+                hit.collider.attachedRigidbody.AddForce(Vector2.up);
+                currentClicked = hit.collider.gameObject;
+                Debug.Log(currentClicked.tag);
+                
+            }
+        }
+    }
+
+    void changeSelected(GameObject Hero)
+    {
+        foreach(GameObject ob in heroes)
+        {
+            ob.GetComponent<MouvementPersonnage>().setSelected(false);
+        }
+        Hero.GetComponent<MouvementPersonnage>().setSelected(true);
+    }
+}
--- Assets/AssetsClassroom/Scripts/MouvementPersonnage.cs
+++ Assets/AssetsClassroom/Scripts/MouvementPersonnage.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class MouvementPersonnage : Character {
+[System.Serializable]
+public class MouvementPersonnage : Character
+{
     Vector3 posPlayer;                                // For movement
     float speedPlayer = 2.0f;                         // Speed of movement
     int HPPlayer = 100;
@@ -12,24 +13,30 @@ public class MouvementPersonnage : Character {
     int portéePlayer = 2;
     int PMPlayer = 3;
     int PAPlayer = 2;
-
+    bool isSelected = false;
+    
     // Use this for initialization
     void Start()
     {
         posPlayer = transform.position; // Take the initial position
-        
+
+    }
+
+    public void setSelected(bool b)
+    {
+        isSelected = b;
     }
 
     // Update is called once per frame
-    void Update () {
-        int tester = targetNumber(2);
-        print(tester);
+    void Update()
+    {
+
         if (Input.GetKey(KeyCode.Q) && transform.position == posPlayer)
         {        // Left
             posPlayer += Vector3.left;
 
             GetComponent<Animator>().Play("WalkingLeft");
-           //GetComponent<Animator>().Play("StandingLeft");
+            //GetComponent<Animator>().Play("StandingLeft");
         }
         if (Input.GetKey(KeyCode.D) && transform.position == posPlayer)
         {        // Right
@@ -50,11 +57,11 @@ public class MouvementPersonnage : Character {
             //GetComponent<Animator>().Play("StandingDown");
 
             //GetComponent<Animator>().SetTrigger("StandLeft");
-            
+
             GetComponent<Animator>().SetTrigger("WalkLeft");
-            if(PMPlayer <= 0)
+            if (PMPlayer <= 0)
             {
-                print("T'es mauvais Jack");
+                Debug.Log("T'es mauvais Jack");
             }
             PMPlayer = PMPlayer - 1;
         }
@@ -65,7 +72,7 @@ public class MouvementPersonnage : Character {
             GetComponent<Animator>().SetTrigger("WalkRight");
             if (PMPlayer <= 0)
             {
-                print("T'es nul Jack");
+                Debug.Log("T'es nul Jack");
             }
             PMPlayer = PMPlayer - 1;
         }
@@ -76,7 +83,7 @@ public class MouvementPersonnage : Character {
             GetComponent<Animator>().SetTrigger("WalkUp");
             if (PMPlayer <= 0)
             {
-                print("T'es moche Jack");
+                Debug.Log("T'es moche Jack");
             }
             PMPlayer = PMPlayer - 1;
         }
@@ -87,7 +94,7 @@ public class MouvementPersonnage : Character {
             GetComponent<Animator>().SetTrigger("WalkDown");
             if (PMPlayer <= 0)
             {
-                print("T'es mort Jack");
+                Debug.Log("T'es mort Jack");
             }
             PMPlayer = PMPlayer - 1;
         }
@@ -102,20 +109,24 @@ public class MouvementPersonnage : Character {
             //AttackTarget(Target);
             //if (PA <= 0)
             //{
-            //    print("T'es faible Jack");
+            //    Debug.Log("T'es faible Jack");
             //}
             //PA = PA - 1;
-           
+
         }
+
+       
     }
 
     void AttackTarget(GameObject theTarget)
     {
-        if (isThereAnEnnemyToAttack()) { print("JOHN CENA"); theTarget.GetComponent<Ennemy>().getDamaged(AttackPlayer); 
-            
-            
+        if (isThereAnEnnemyToAttack())
+        {
+            Debug.Log("JOHN CENA"); theTarget.GetComponent<Ennemy>().getDamaged(AttackPlayer);
+
+
         };
-        
+
     }
 
     public GameObject[] whoCanIAttack()
@@ -123,9 +134,10 @@ public class MouvementPersonnage : Character {
         GameObject[] ennemies = GameObject.FindGameObjectsWithTag("Ennemy");
         GameObject[] possibleTargets = new GameObject[targetNumber(portéePlayer)];
         int nbTargets = 0;
-        foreach(GameObject ob in ennemies)
+        foreach (GameObject ob in ennemies)
         {
-            if ((Vector3.Distance(ob.GetComponent<Ennemy>().getPos(), posPlayer) <= portéePlayer)){
+            if ((Vector3.Distance(ob.GetComponent<Ennemy>().getPos(), posPlayer) <= portéePlayer))
+            {
                 possibleTargets[nbTargets] = ob;
                 nbTargets++;
             }
@@ -136,12 +148,12 @@ public class MouvementPersonnage : Character {
     public bool isThereAnEnnemyToAttack()
     {
         GameObject[] ennemies = GameObject.FindGameObjectsWithTag("Ennemy");
-       
+
         foreach (GameObject ob in ennemies)
         {
             if ((Vector3.Distance(ob.GetComponent<Ennemy>().getPos(), posPlayer) <= portéePlayer))
             {
-               return true;
+                return true;
             }
         }
         return false;
@@ -150,10 +162,10 @@ public class MouvementPersonnage : Character {
     public int targetNumber(int range)
     {
         int targetNb = 0;
-        for(int i = 1; i <= range; i++)
+        for (int i = 1; i <= range; i++)
         {
             targetNb = targetNb + (i * 4);
-            print(targetNb);
+
         }
         return targetNb;
     }
