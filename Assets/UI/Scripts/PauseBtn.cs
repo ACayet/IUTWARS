d3934654e2ca8e3d0588da2f87c0b59bf9747e79
Loutrinator <antoine.pavy92@gmail.com>
2018-03-27 16:38:41 +0200

    pause
    fdfg

---
+++ Assets/UI/Scripts/PauseBtn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBtn : MonoBehaviour {
+    [SerializeField]
+    float min;
+    [SerializeField]
+    float offset;
+    float posX;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
