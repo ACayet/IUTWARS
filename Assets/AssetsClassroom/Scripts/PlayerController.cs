7718740ce0518fa9572e84af6719acdba60751ec
PaulF <paulfabre2009@hotmail.fr>
2018-03-29 17:19:56 +0200

    Nouvelle animation de déplacement et spawn au bons endroits

---
+++ Assets/AssetsClassroom/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+
+    private Animator anim;
+    private Rigidbody2D myRigidbody;
+
+    private bool playerMoving;
+    private Vector2 lastMove;
+
+    private static bool playerExists;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        if (!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+
+
+
+
+    }
+}
