febd2f9aa768a3a0267d370bc518eb79d9754352
Villem_p <pvillemain@hotmail.fr>
2018-04-04 15:37:41 +0200

    nB

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(GameObject.FindGameObjectWithTag("PlayerMap") != null)
+        {
+            GameObject.FindGameObjectWithTag("PlayerMap").GetComponent<MouvementPersonnage>().getOutOfScene();
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
