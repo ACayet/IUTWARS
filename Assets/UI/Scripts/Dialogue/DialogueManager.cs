4f05f444a2f17b712ec871c3deb92ff4ad1df5b2
Villem_p <pvillemain@hotmail.fr>
2018-04-05 08:34:47 +0200

    scene intro

--- Assets/UI/Scripts/Dialogue/DialogueManager.cs
+++ Assets/UI/Scripts/Dialogue/DialogueManager.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class DialogueManager : MonoBehaviour {
 
     public Text TextDialogue;
     public Text TextNom;
     public Queue<string> phrases;
+    public Dialogue dialogue;
+    public GameObject btnC;
+
 
     public Animator animator;
 
 	// Use this for initialization
 	void Start () {
         phrases = new Queue<string>();
-	}
+        StartDialogue(dialogue);
+    }
+
     public void StartDialogue(Dialogue dialogue)
     {
         animator.SetBool("IsOpen", true);
@@ -39,6 +45,9 @@ public class DialogueManager : MonoBehaviour {
     public void EndDialogue()
     {
         animator.SetBool("IsOpen", false);
+        SceneManager.LoadScene("Couloir_Carte");
         Debug.Log("Fin de la conversation.");
     }
+
+    
 }
